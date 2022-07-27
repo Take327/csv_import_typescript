18c5b8f7c38913b824a74e513d22738ab46eef06
Take327 <gatewayseed2@gmail.com>
2022-07-27 22:44:29 +0900

    エラー処理変更

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import fs from 'fs';
 
 const main = () => {
+    let ERROR;
     try {
+        ERROR = 'E00001';
         const inputPaths: string[] = process.argv.slice(2);
         // 入力されているファイルパスが2つあるか確認する
         if (inputPaths.length !== 2) {
             throw new Error('CSVファイルのパスを2つ入力してください');
         }
 
+        ERROR = 'E00002';
         const pathA = inputPaths[0];
         const pathB = inputPaths[1];
 
+        ERROR = 'E00003';
         // 第一ファイルチェック
-        let error = '';
-        error = inputPathChack(pathA);
-        if (error !== '') {
-            throw new Error(error);
+        let errorMessage = '';
+        errorMessage = inputPathChack(pathA);
+        if (errorMessage !== '') {
+            console.log(errorMessage);
+            return;
         }
 
+        ERROR = 'E00004';
         // 第二ファイルチェック
-        error = inputPathChack(pathB);
-        if (error !== '') {
-            throw new Error(error);
+        errorMessage = inputPathChack(pathB);
+        if (errorMessage !== '') {
+            console.log(errorMessage);
+            return;
         }
 
+        ERROR = 'E00005';
         // ファイルを読み込む 文字コードを指定するとStringで返却される
         const textA = fs.readFileSync(pathA, 'utf-8');
         const textB = fs.readFileSync(pathB, 'utf-8');
@@ -31,7 +39,8 @@ const main = () => {
         console.log('textA', textA);
         console.log('textB', textB);
     } catch (e) {
-        console.log(e);
+        console.error('エラーコード:', ERROR);
+        console.error(e);
     }
 };
 
